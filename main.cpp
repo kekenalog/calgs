2abb70e3befeb2f97f4c7d19fa6d3d3ae02e7e6d
ywang <yong.wang@ucarinc.com>
2017-10-09 14:33:16 +0800

    测试数据生成器

--- main.cpp
+++ main.cpp
@@ -23,32 +23,14 @@ void selectionSort(T arr[], int n) {
 
 int main() {
 
-    // 测试模版函数,传入整型数组
-    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    selectionSort(a, 10);
-    for (int i = 0; i < 10; i++)
-        cout << a[i] << " ";
-    cout << endl;
+    //测试排序算法辅助函数
+    int N = 20000;
 
-    // 测试模版函数,传入浮点型数组
-    float b[4] = {4.4, 3.3, 2.2, 1.1};
-    selectionSort(b, 4);
-    for (int i = 0; i < 4; i++)
-        cout << b[i] << " ";
-    cout << endl;
+    int *arr = SortTestHelper::generateRandomArray(N, 0, 100000);
+    selectionSort(arr, N);
+    SortTestHelper::printArray(arr, N);
+    delete arr;
 
-    // 测试模板函数，传入字符串数组
-    string c[4] = {"D", "C", "B", "A"};
-    selectionSort(c, 4);
-    for (int i = 0; i < 4; i++)
-        cout << c[i] << " ";
-    cout << endl;
-
-    // 测试模板函数，传入自定义结构体Student数组
-    Student d[4] = { {"D",90} , {"C",100} , {"B",95} , {"A",95} };
-    selectionSort(d,4);
-    for( int i = 0 ; i < 4 ; i ++ )
-        cout<<d[i];
-    cout<<endl;
     return 0;
+
 }
