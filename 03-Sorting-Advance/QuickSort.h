e755e2f769a349a95fe5d43ac82680625d0464ab
ywang <yong.wang@ucarinc.com>
2017-10-14 19:38:12 +0800

    双路排序

--- 03-Sorting-Advance/QuickSort.h
+++ 03-Sorting-Advance/QuickSort.h
@@ -34,6 +34,44 @@ int __partition(T arr[], int l, int r){
     return j;
 }
 
+// 双路快速排序的partition
+// 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+template <typename T>
+int _partition2(T arr[], int l, int r){
+
+    // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+    swap( arr[l] , arr[rand()%(r-l+1)+l] );
+    T v = arr[l];
+
+    // arr[l+1...i) <= v; arr(j...r] >= v
+    int i = l+1, j = r;
+    while( true ){
+        // 注意这里的边界, arr[i] < v, 不能是arr[i] <= v
+        // 思考一下为什么?
+        while( i <= r && arr[i] < v )
+            i ++;
+
+        // 注意这里的边界, arr[j] > v, 不能是arr[j] >= v
+        // 思考一下为什么?
+        while( j >= l+1 && arr[j] > v )
+            j --;
+
+        // 对于上面的两个边界的设定, 有的同学在课程的问答区有很好的回答:)
+        // 大家可以参考: http://coding.imooc.com/learn/questiondetail/4920.html
+
+        if( i > j )
+            break;
+
+        swap( arr[i] , arr[j] );
+        i ++;
+        j --;
+    }
+
+    swap( arr[l] , arr[j]);
+
+    return j;
+}
+
 // 对arr[l...r]部分进行快速排序
 template <typename T>
 void __quickSort(T arr[], int l, int r){
@@ -43,7 +81,8 @@ void __quickSort(T arr[], int l, int r){
         return;
     }
 
-    int p = __partition(arr, l, r);
+    int p = _partition2(arr, l, r);
+//    int p = __partition(arr, l, r);
     __quickSort(arr, l, p-1 );
     __quickSort(arr, p+1, r);
 }
